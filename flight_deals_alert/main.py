ed428f4c28ca06e83e782b176540951876204105
Shivam Kumar <119096144+Shivam29k@users.noreply.github.com>
2023-07-02 22:47:02 +0530

    Flight Club

--- flight_deals_alert/main.py
+++ flight_deals_alert/main.py
@@ -1,3 +1,6 @@
+
+#  url to update the member in flight club datasheet:-- https://replit.com/@ShivamKumar28/Shivam-Flight-Club
+
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
 from data_manager import DataManager
 from flight_search import FlightSearch
@@ -12,8 +15,10 @@ for destination in datamager.data:
         ORIGIN_CITY_IATA,
         destination["iataCode"],
     )
+    if flight is None: continue
     if flight.price < destination["lowestPrice"]:
         message = f"Low price alert! Only ₹{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}"
         print(message)
-        send_notification.send_sms(message)
-        send_notification.send_mail(message)
+        # send_notification.send_sms(message)
+        # send_notification.send_mail(message)
+        send_notification.send_mails(message)
--- flight_deals_alert/notification_manager.py
+++ flight_deals_alert/notification_manager.py
@@ -1,4 +1,5 @@
 from twilio.rest import Client
+import requests
 import smtplib
 import os
 from dotenv import load_dotenv
@@ -12,15 +13,17 @@ TO_NO = os.getenv("to_phone_no")
 MY_MAIL = os.getenv("my_email")
 MAIL_PASS = os.getenv("password")
 
+SHEETRY_GET_USERS = "https://api.sheety.co/7e5dc26de00c4f9595d05537d7febfae/flightDeals/users"
+AUTH = os.getenv("sheety_authorization")
 
 class NotificationManager:
-    def send_mail(self, text):
+    def send_mail(self, text, mail):
         with smtplib.SMTP("smtp.gmail.com", 587) as connection:
             connection.starttls()
             connection.login(user=MY_MAIL, password=MAIL_PASS)
             connection.sendmail(
                 from_addr=MY_MAIL,
-                to_addrs=MY_MAIL,
+                to_addrs=mail,
                 msg=(f"Subject: Price Alert !! \n\n{text}").encode("utf-8")
             )
         print("mail sent")
@@ -34,4 +37,10 @@ class NotificationManager:
         )
         print("sms sent")
 
-send_notification = NotificationManager()
+    def send_mails(self, text):
+        head = {"Authorization": AUTH, "Content-Type": "application/json"}
+        response = requests.get(url=SHEETRY_GET_USERS, headers=head)
+        # print(response.json())
+        for data in response.json()['users']:
+            mail = data["email"]
+            self.send_mail(text, mail)
