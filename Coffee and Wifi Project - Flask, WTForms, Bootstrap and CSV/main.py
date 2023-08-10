c1b42288ae48c2e1a695fb3d523c0c00bc7b6ef9
Shivam Kumar <119096144+Shivam29k@users.noreply.github.com>
2023-08-10 22:31:48 +0530

    Coffee nd Wifi review and store review

---
+++ Coffee and Wifi Project - Flask, WTForms, Bootstrap and CSV/main.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, URLField, TimeField, SelectField
+from wtforms.validators import DataRequired, URL
+import csv
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
+
+
+class CafeForm(FlaskForm):
+    cafe = StringField('Cafe name', validators=[DataRequired()])
+    location_url = URLField('Location Url', validators=[DataRequired(), URL()])
+    open_time = TimeField('Opening Time', validators=[DataRequired()])
+    close_time = TimeField('Closing Time', validators=[DataRequired()])
+    coffee_rating = SelectField('Coffee Rating', choices=["☕️", "☕☕", "☕☕☕", "☕☕☕☕", "☕☕☕☕☕"], validators=[DataRequired()])
+    wifi_rating = SelectField('Wifi Rating', choices=["✘", "💪", "💪💪", "💪💪💪", "💪💪💪💪", "💪💪💪💪💪"], validators=[DataRequired()])
+    power_rating = SelectField('Power Outlet Rating', choices=["✘", "🔌", "🔌🔌", "🔌🔌🔌", "🔌🔌🔌🔌", "🔌🔌🔌🔌🔌"], validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+# all Flask routes below
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_cafe():
+    form = CafeForm()
+    if form.validate_on_submit():
+        print("True")
+        data = request.form
+        row = {
+            'Cafe Name': data['cafe'],
+            'Location': data['location_url'],
+            'Open' : data['open_time'],
+            'Close': data['close_time'],
+            'Coffee': data['coffee_rating'],
+            'Wifi': data['wifi_rating'],
+            'Power' : data['power_rating']
+        }
+
+        with open('cafe-data.csv','a+', encoding='utf-8') as file:
+            writer = csv.DictWriter(file, fieldnames=['Cafe Name', 'Location', 'Open', 'Close', 'Coffee', 'Wifi', 'Power'])
+            writer.writerow(row)
+        return redirect(url_for('cafes'))
+    return render_template('add.html', form=form)
+
+
+@app.route('/cafes')
+def cafes():
+    with open('cafe-data.csv', newline='', encoding='utf-8') as csv_file:
+        csv_data = csv.reader(csv_file, delimiter=',')
+        list_of_rows = []
+        for row in csv_data:
+            list_of_rows.append(row)
+    return render_template('cafes.html', cafes=list_of_rows)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
