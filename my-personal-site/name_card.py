5ab38944ad9851c0bab5d6a8b123e35875f98f8c
Shivam Kumar <119096144+Shivam29k@users.noreply.github.com>
2023-10-22 11:36:33 +0530

    minor commit

--- my-personal-site/name_card.py
+++ my-personal-site/name_card.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('name_card_index.html')
+    # return render_template('index.html')
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
 
 if '__main__' == __name__:
     app.run(debug=True)
